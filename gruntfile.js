5c80de57b4c267c720ee308061d65eac39557c86
Ryan Bowman <ryan@circle.co.ke>
2014-03-18 15:13:56 +0000

    added watch and sass to gruntfile, put in directory strucutre, put in reset for css

--- gruntfile.js
+++ gruntfile.js
@@ -1,8 +1,75 @@
-{
-  "name": "example-project",
-  "version": "0.1.0",
-  "devDependencies": {
-    "grunt": "~0.4.1",
-    "grunt-contrib-concat": "~0.3.0"
-  }
-}
+module.exports = function(grunt) {
+
+    // 1. All configuration goes here 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            dist: {
+                src: [
+                    'cms/wp-content/themes/timby/js/libs/*.js', // All JS in the libs folder
+                    'cms/wp-content/themes/timby/js/*.js'  // This specific file
+                ],
+                dest: 'cms/wp-content/themes/timby/js/build/production.js',
+            }
+        },
+
+        uglify: {
+            build: {
+                src:  'cms/wp-content/themes/timby/js/build/production.js',
+                dest: 'cms/wp-content/themes/timby/js/build/production.min.js'
+            }
+        },
+
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'cms/wp-content/themes/timby/images/',
+                    src: ['*.{png,jpg,gif}'],
+                    dest: 'cms/wp-content/themes/timby/images/build/'
+                }]
+            }
+        },
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'cms/wp-content/themes/timby/css/global.css': 'cms/wp-content/themes/timby/sass/global.sass'
+                }
+            } 
+        },
+
+        watch: {
+            scripts: {
+                files: ['js/*.js'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                    spawn: false,
+                }
+            },
+            css: {
+                files: ['cms/wp-content/themes/timby/sass/global.sass', 'cms/wp-content/themes/timby/sass/modules/*'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false,
+                }
+            }
+        }
+
+    });
+
+    // 3. Where we tell Grunt we plan to use plug-ins.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['concat','uglify','imagemin','sass','watch']);
+
+};
