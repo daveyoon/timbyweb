16eb4823b3ff8f7e7c32669896468f3c62d48c31
Kamweti Muriuki <kamwetimuriuki@gmail.com>
2014-03-20 17:40:11 +0300

    cronjobs modification, created a cpt devices

--- cms/wp-content/themes/timby/inc/custom-post-types.php
+++ cms/wp-content/themes/timby/inc/custom-post-types.php
@@ -4,6 +4,7 @@
 [Custom Post Types]
 
 * Report
+* Devices
 -------------------------------------------------------------------*/
 
 add_action( 'init', 'register_custom_post_type_report' );
@@ -21,7 +22,7 @@ function register_custom_post_type_report() {
         'not_found' => _x( 'No report found', 'timbyweb' ),
         'not_found_in_trash' => _x( 'No report found in Trash', 'timbyweb' ),
         'parent_item_colon' => _x( 'Parent report:', 'timbyweb' ),
-        'menu_name' => _x( 'Reports', 'timbyweb' ),
+        'menu_name' => _x( 'Reports', 'timbyweb' )
     );
 
     $args = array( 
@@ -43,9 +44,54 @@ function register_custom_post_type_report() {
         'query_var' => true,
         'can_export' => true,
         'rewrite' => true,
-        'capability_type' => 'post'
+        'capability_type' => 'post',
+        'menu_icon' => 'dashicons-format-status'
     );
 
     register_post_type( 'report', $args );
+
+}
+
+add_action( 'init', 'register_custom_post_type_phone' );
+function register_custom_post_type_phone() {
+
+    $labels = array( 
+        'name' => _x( 'Phones', 'timbyweb' ),
+        'singular_name' => _x( 'Phone', 'timbyweb' ),
+        'add_new' => _x( 'Add New', 'timbyweb' ),
+        'add_new_item' => _x( 'Add a new phone', 'timbyweb' ),
+        'edit_item' => _x( 'Edit phone', 'timbyweb' ),
+        'new_item' => _x( 'New phone', 'timbyweb' ),
+        'view_item' => _x( 'View phone', 'timbyweb' ),
+        'search_items' => _x( 'Search Phone', 'timbyweb' ),
+        'not_found' => _x( 'No phone found', 'timbyweb' ),
+        'not_found_in_trash' => _x( 'No phone found in Trash', 'timbyweb' ),
+        'parent_item_colon' => _x( 'Parent phone:', 'timbyweb' ),
+        'menu_name' => _x( 'Phone Devices', 'timbyweb' ),
+    );
+
+    $args = array( 
+        'labels' => $labels,
+        'hierarchical' => false,
+        'description' => 'Devices on the ground',
+        'supports' => array( 'title', 'editor', 'excerpt', 'custom-fields'),
+        
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        
+        
+        'show_in_nav_menus' => true,
+        'publicly_queryable' => true,
+        'exclude_from_search' => true,
+        'has_archive' => false,
+        'query_var' => true,
+        'can_export' => true,
+        'rewrite' => true,
+        'capability_type' => 'post',
+        'menu_icon' => 'dashicons-smartphone'
+    );
+
+    register_post_type( 'phone', $args );
 
 }
