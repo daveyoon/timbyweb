00204eec56b8cb3bda16dddd07c22f1bbe438542
Ryan Bowman <ryan@circle.co.ke>
2014-04-07 17:50:17 +0100

    adding m4a m4b and m4p audio

--- cms/wp-content/themes/timby/js/controllers.js
+++ cms/wp-content/themes/timby/js/controllers.js
@@ -399,14 +399,14 @@ angular.module('timby.controllers', [])
               'video/x-matroska'
             ])
         ){
-        $scope.invalid.video = 'Sorry we can only accept x-flv, mp4, x-troff-msvideo, av, msvideo, x-msvideo and x-matroska video files.';
+        $scope.invalid.video = 'Sorry we can only accept .mov, .mp4, .avi and .mkv video files.';
         return;
       }
       $scope.report.video = $files;
     }
 
     if( $type == 'audio'){
-      if( ! files_are_valid($files, ['audio/mp3','audio/mp4','audio/m4a', 'audio/ogg']) ){
+      if( ! files_are_valid($files, ['audio/mp3', 'audio/mp4', 'audio/mp4a-latm', 'audio/mp4a-latm', 'audio/mp4a-latm', 'audio/ogg']) ){
         $scope.invalid.audio = 'Sorry we can only accept mp3, mp4, m4a and ogg audio files.';
         return;
       }
