c9017b09eb5f9121786ecdbcca4cc2eeb7dff7d0
Kamweti Muriuki <kamwetimuriuki@gmail.com>
2014-04-23 18:14:03 +0300

    localized the config object

--- cms/wp-content/themes/timby/header.php
+++ cms/wp-content/themes/timby/header.php
@@ -16,11 +16,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width,initial-scale=1.0">
         <meta name="description" content="<?php bloginfo('description'); ?>">
-        <script type="text/javascript">
-            window.Config = <?php echo get_timby_options_json(); ?>;
-        </script>
 
-<?php wp_head(); ?>
+    <?php wp_head(); ?>
 
     </head>
     <body <?php body_class(); ?>>
